1018669a6174209fb7ba66f2264d0abae5b5af1f
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-03-25 21:21:13 +0100

    make pygraphviz optional and add tests_require

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from setuptools import setup
 
 version = '0.1'
 
+optional_requires = [
+    'pygraphviz'
+]
+
+tests_require = [
+    'nose', 'mock'
+]
+
 entry_points = {
     'console_scripts': [
         'import-tree = import_tree.import_tree:main'
@@ -13,8 +21,8 @@ setup(
     author="Andrea Crotti",
     author_email="andrea.crotti.0gmail.com",
     version=version,
+    tests_require = tests_require,
     packages=['import_tree'],
-    install_requires=['pygraphviz'],
     description="generate the import tree",
     entry_points=entry_points,
     )
